1ec98bb9655b0b491ab5706d7e42f08aeba23466
Vibeo Dev <vibeo@app.com>
2026-04-14 23:30:09 +0200

    Firebase web desteği eklendi (appId + authDomain)

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,9 +1,10 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
-    show defaultTargetPlatform, TargetPlatform;
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
+    if (kIsWeb) return web;
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
         return android;
@@ -15,6 +16,15 @@ class DefaultFirebaseOptions {
         );
     }
   }
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyCJJsjVpJjhD5WYmGx7fI3oqVjzc3Nwwzg',
+    appId: '1:74675326225:web:f34c53800ce6cb16abd0b0',
+    messagingSenderId: '74675326225',
+    projectId: 'vibeo-58032',
+    authDomain: 'vibeo-58032.firebaseapp.com',
+    storageBucket: 'vibeo-58032.firebasestorage.app',
+  );
 
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyBXeORIk64WNqHdsEpDY-iMpDSjVn47jYo',
